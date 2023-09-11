39d294a5dc92c416424486f250c6209df872ad0c
duncan <duncan.reijersen@student.hu.nl>
2023-09-11 18:44:55 +0200

    first loop learned

--- pythonProject1/stationzuil.py
+++ pythonProject1/stationzuil.py
@@ -1,7 +1,25 @@
-while True:
+while True:  # zo lang dit klopt
     message = str(input("would you like to leave a message. if so please type yes "))
     if message != str('yes'):
+        # als dit niet gelijk aanelkaar is dan repeat hij de statment daar boven weer  wegens de continue functie
         continue
     else:
-        name = str(input('what is your name? if the name that is chosen is NON it will become anonymously '))
-        break
+        name = str(input('what is your name? if the name that is chosen is NON it will become anonymous '))
+    break
+
+true_message = str(input('than please leave a massage'))
+        # het idee hier is om de naam op te slaan samen met ze uitspraak de message alleen daarvoor is waarschijnlijk een database
+        # handig
+        #  nee = ('non', 'NON')
+        #  if name == str(nee)
+        # save the name anonymous
+        #  else :
+        # save the name input
+        # save massage next to the name
+
+
+
+
+
+
+
