4520ff84ae25a566886ef232254866d87efae012
duncan <duncan.reijersen@student.hu.nl>
2023-09-11 17:23:24 +0200

    hi

---
+++ pythonProject1/stationzuil.py
@@ -0,0 +1,10 @@
+user = input('name')
+print(user)
+message = input('would you like to leave a message, please type yes or no')
+if str(message) == "yes" :
+ pass
+elif str(message) == "no" :
+    
+
+
+print(message)
