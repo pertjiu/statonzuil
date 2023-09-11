862cde51335750ec7f68eaa29a2c6e130eb534b9
duncan <duncan.reijersen@student.hu.nl>
2023-09-11 17:58:54 +0200

    first loop learned

--- pythonProject1/stationzuil.py
+++ pythonProject1/stationzuil.py
@@ -1,10 +1,10 @@
-user = input('name')
-print(user)
-message = input('would you like to leave a message, please type yes or no')
-if str(message) == "yes" :
- pass
-elif str(message) == "no" :
+while True:
+ user = input('name')
+  print(user)
+   message = input('would you like to leave a message, please type yes or no')
+ if str(message) == "yes" :
+  break
     
 
 
-print(message)
+# print(message)
