1d81158009c805a562a8ff1e48462156e636475c
duncan <duncan.reijersen@student.hu.nl>
2023-09-12 12:19:54 +0200

    time delay toe gevoegd

--- pythonProject1/stationzuil.py
+++ pythonProject1/stationzuil.py
@@ -1,18 +1,25 @@
+import time
+
 while True:  # zo lang dit klopt
     message = str(input("would you like to leave a message. if so please type yes "))
     if message != str('yes'):
         # als dit niet gelijk aanelkaar is dan repeat hij de statment daar boven weer  wegens de continue functie
         continue
     else:
-        name = str(input('what is your name? if the name that is chosen is NON it will become anonymous '))
-    break
+        name = str(input('what is your name? if the name that is chosen is N it will become anonymous '))
+    nee = ('n', 'N')
+    # if name == nee :
+    # save name als anonymous
+    # else:
+    # save given name
+    true_message = str(input('than please leave a massage '))
+    # het idee hier is om de naam op te slaan samen met ze uitspraak de message
+    # alleen daarvoor is waarschijnlijk een database handig
+    print('thanks for leaving a massage and have a nice travel')
+    time.sleep(2) # dit vertraagt het moment van de print functie naar de continue functie
+    continue
 
-true_message = str(input('than please leave a massage '))
-# het idee hier is om de naam op te slaan samen met ze uitspraak de message
-# alleen daarvoor is waarschijnlijk een database handig
-#  nee = ('non', 'NON')
-#  if name == str(nee)
-# save the name anonymous
-#  else :
-# save the name input
-# save massage next to the name
+
+
+
+
