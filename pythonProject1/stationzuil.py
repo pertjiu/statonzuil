af237a267beec9f6ac25194532e505ad8ab8ee20
duncan <duncan.reijersen@student.hu.nl>
2023-09-18 10:00:14 +0200

    time delay toe gevoegd psodo toegevoegd

--- pythonProject1/stationzuil.py
+++ pythonProject1/stationzuil.py
@@ -1,6 +1,8 @@
 import time
+import datetime
 
-while True:  # zo lang dit klopt
+
+while True:
     message = str(input("would you like to leave a message. if so please type yes "))
     if message != str('yes'):
         print('have a nice day')
@@ -9,13 +11,16 @@ while True:  # zo lang dit klopt
     else:
         name = str(input('what is your name? if the name that is chosen is N it will become anonymous '))
     nee = ('n', 'N')
-    # if name == nee :
-    # save name als anonymous
-    # else:
-    # save given name
+    user = [name]
+    if name in nee:
+        user[0] = "anonymous"
+        # print(user)
+    else:
+        user[0] = str(name)
+        print(user)
     while True:
         true_message = str(input('than please leave a massage of max 140 letters '))
-        if len(true_message) > 10:
+        if len(true_message) > 140:
             print('that is more than 140 letters')
             time.sleep(1)
             print("please type it again")
@@ -25,9 +30,13 @@ while True:  # zo lang dit klopt
             print('thanks for leaving a massage and have a nice travel')
             time.sleep(2)
         break
+    smassage = ["placeholder"]
+    smassage[0] = str(true_message)
+
+    now = datetime.datetime.now()
+    cdt = [0]
+    cdt[0] = str(now.strftime("%H:%M:%S %Y:%m:%d"))
+    print(cdt)
+
     continue
-# het idee hier is om de naam op te slaan samen met ze uitspraak de message plus de tijd en datum van de message
-    # alleen daarvoor is waarschijnlijk een database handig
-
-
 
--- pythonProject2/psodocode.py
+++ pythonProject2/psodocode.py
@@ -1,5 +1,9 @@
-#( inpoort time zorgt voor een mogelijkheid om code te delayen zodat het wat netter overkomt daar naast word er een
+#import time zorgt voor een mogelijkheid om code te delayen zodat het wat netter overkomt daar naast word er een
 #while True loop gebreukt zodat er weer terug geloopt kan worden zolang het programma runt
 # er zit ook al lichte moderatie in in de eerste vraag of ze een bericht achtere willen laten
 # mis zou je denken dat dit niet nodig is maar uit ervaring lezen mensen vaak niet en dit haalt al de idiote weg
 # uit de echte moderatie
+
+# import datetime voor de library en dan de now functie zorgt ervoor dat hij gaat kijken naar de datum en tijd op dat
+# moment. alleen kan je er dan nog niet bij daar voor word dan now strife gebruikt waar bij hij dan string symbools
+# herkent en dan good weet intedelen waardoor de waardes van de funcite now goed omgezet word naar de jouw gekozen volgorden
