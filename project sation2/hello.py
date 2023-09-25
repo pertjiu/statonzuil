465029f62f4acbe806ac7ef065436438b164d3db
duncan <duncan.reijersen@student.hu.nl>
2023-09-25 09:28:17 +0200

    this is the official version

---
+++ project sation2/hello.py
@@ -0,0 +1,11 @@
+# 10-9 import time zorgt voor een mogelijkheid om code te delayen zodat het wat netter overkomt daar naast word er een
+#while True loop gebreukt zodat er weer terug geloopt kan worden zolang het programma runt
+# er zit ook al lichte moderatie in in de eerste vraag of ze een bericht achtere willen laten
+# mis zou je denken dat dit niet nodig is maar uit ervaring lezen mensen vaak niet en dit haalt al de idiote weg
+# uit de echte moderatie
+
+# 18-9 import datetime voor de library en dan de now functie zorgt ervoor dat hij gaat kijken naar de datum en tijd op dat
+# moment. alleen kan je er dan nog niet bij daar voor word dan now strife gebruikt waar bij hij dan string symbools
+# herkent en dan good weet intedelen waardoor de waardes van de funcite now goed omgezet word naar de jouw gekozen volgorden
+
+# 19-9 txt files toegevoegt \n soorgt voor enter in het txt bestand en met
