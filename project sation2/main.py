24e963c0187df0c6036a3d1ec5a89b097c2c6ee6
duncan <duncan.reijersen@student.hu.nl>
2023-10-02 09:40:47 +0200

    this is the official version

--- project sation2/main.py
+++ project sation2/main.py
@@ -2,6 +2,7 @@ import time
 import datetime
 import random
 
+
 while True:
     message = str(input("would you like to leave a message. if so please type yes "))
     if message != str('yes'):
@@ -12,13 +13,13 @@ while True:
         time.sleep(1)
         name = str(input('what is your name? if the name that is chosen is N it will become anonymous '))
     nee = ('n', 'N')
-    user = [name]
+    # user = [name]
     if name in nee:
-        user[0] = "anonymous"
-    else:
-        user[0] = str(name)
-    while True:
+        name = "anonymous"
         time.sleep(1)
+    else:
+        time.sleep(1)
+    while True:
         true_message = str(input('than please leave a massage of max 140 letters '))
         if len(true_message) > 140:
             print('that is more than 140 letters')
@@ -36,8 +37,7 @@ while True:
     smassage[0] = str(true_message)
 
     now = datetime.datetime.now()
-    cdt = [0]
-    cdt[0] = str(now.strftime("%D %T"))
+    cdt = str(now.strftime("%D %T"))
     # print(cdt)
 
     infile = open("stations.txt", "r")
@@ -45,13 +45,9 @@ while True:
     infile.close()
 
     wordlist = content.split()
-    station = [0]
-    station[0] = str(random.choice(wordlist))
+    station = str(random.choice(wordlist))
 
     outfile = open("gev stationzeill.txt", "a")
-    outfile.write(str(user))
-    outfile.write(str(smassage))
-    outfile.write(str(cdt))
-    outfile.write(str(station) + '\n')
+    hello = f"{name} {true_message} {cdt} {station}" + '\n'
+    outfile.write(hello)
     outfile.close()
-    continue
