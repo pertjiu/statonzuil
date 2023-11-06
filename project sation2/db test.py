3707b3d1498bfa8b8eef5c62b7b052f3fde7d54b
duncan <duncan.reijersen@student.hu.nl>
2023-11-06 11:27:43 +0100

    fixed module 2 voor herkenning van moderator

--- project sation2/db test.py
+++ project sation2/db test.py
@@ -13,6 +13,70 @@ result = []
 dbgoedkeuring = []
 moderatorname = input('name ')
 moderatoremail = input('email ')
+dbgoedkeuring.append(f"{moderatorname}, {moderatoremail}")
+
+for record in dbgoedkeuring:
+    print(record)
+
+conn = psycopg2.connect(
+    host="20.77.182.19",
+    database='stationszuil',
+    user='postgres',
+    password="FD1ns81g02!!"
+)
+cursor = conn.cursor()
+
+query = """SELECT moderator_id, E_mail
+               FROM moderator;"""
+cursor.execute(query)
+rows = cursor.fetchall()
+
+email_list = [email for _, email in rows]
+
+conn.close()
+
+conn = psycopg2.connect(
+            host="20.77.182.19",
+            database='stationszuil',
+            user='postgres',
+            password="FD1ns81g02!!"
+        )
+if moderatoremail in email_list:
+    print("Email is valid for moderation.")
+else:
+    for record in dbgoedkeuring:
+        cursor = conn.cursor()
+        query = """
+            INSERT INTO moderator (naam, E_mail)
+            VALUES (%s, %s);"""
+        cursor.execute(query, tuple(record.split(', ')))
+    conn.commit()
+    conn.close()
+
+
+conn = psycopg2.connect(
+    host="20.77.182.19",
+    database='stationszuil',
+    user='postgres',
+    password="FD1ns81g02!!"
+)
+cursor = conn.cursor()
+
+query = """SELECT moderator_id, E_mail
+           FROM moderator;"""
+cursor.execute(query)
+rows = cursor.fetchall()
+
+
+email_to_moderator_id = {email: moderator_id for moderator_id, email in rows}
+
+if moderatoremail in email_to_moderator_id:
+    moderator_id = email_to_moderator_id[moderatoremail]
+    print(f"Email is valid for moderation. Moderator ID: {moderator_id}")
+else:
+    print("Email is not found in the database.")
+
+conn.close()
 
 for sentence in sentences:
     parts = sentence.split('--')
@@ -32,11 +96,7 @@ for sentence in sentences:
         now = datetime.datetime.now()
         beordeelingD = now.strftime("%D")
         beordeelingT = now.strftime("%T")
-        dbgoedkeuring.append(f"{moderatorname}, {moderatoremail}")
-        result.append(f'{name}, {place}, {message}, {date}, {time}, {beordeelingT}, {beordeelingD}, {goedkeuring}')
-
-for record in dbgoedkeuring:
-    print(record)
+        result.append(f'{name}, {place}, {message}, {date}, {time}, {beordeelingT}, {beordeelingD}, {goedkeuring}, {moderator_id}')
 
 conn = psycopg2.connect(
     host="20.77.182.19",
@@ -46,16 +106,9 @@ conn = psycopg2.connect(
 )
 cursor = conn.cursor()
 
-for record in dbgoedkeuring:
-    query = """
-    INSERT INTO moderator (naam, E_mail)
-    VALUES (%s, %s);"""
-    cursor.execute(query, tuple(record.split(', ')))
-    conn.commit()
-
 for record in result:
-    query = """INSERT INTO bericht(naam, plaats, bericht, datum, tijd, gktijd, gkdatum, goedkeuring)
-                VALUES( %s, %s, %s, %s, %s, %s, %s, %s);"""
+    query = """INSERT INTO bericht(naam, plaats, bericht, datum, tijd, gktijd, gkdatum, goedkeuring, moderator_id)
+                VALUES( %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
     cursor.execute(query, tuple(record.split(', ')))
 
     conn.commit()
